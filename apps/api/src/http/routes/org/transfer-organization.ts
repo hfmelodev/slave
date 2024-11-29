1b40fa06b8c75ffe22ac5b5aa2d4c9cce4afc876
Hilquias Ferreira Melo <hilquiasfmelo@gmail.com>
2024-11-28 21:44:32 -0300

    feat: :sparkles: Rota: Transferir uma organização

---
+++ apps/api/src/http/routes/org/transfer-organization.ts
@@ -0,0 +1,98 @@
+import { organizationSchema } from '@slave/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermission } from '@/utils/get-user-permission'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function transferOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .patch(
+      '/organizations/:slug/owner',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary:
+            'Transferir a propriedade de uma organização para outro membro.',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            transferToUserId: z.string().uuid(),
+          }),
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+
+        const userId = await request.getCurrentUserId()
+        // Busca a organização e o membro atual
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const authOrganization = organizationSchema.parse(organization)
+
+        const { cannot } = getUserPermission(userId, membership.role)
+
+        if (cannot('transfer_ownership', authOrganization)) {
+          throw new UnauthorizedError(
+            'Você não tem permissão para transferir a propriedade dessa organização.',
+          )
+        }
+
+        const { transferToUserId } = request.body
+
+        const transferToMembership = await prisma.member.findUnique({
+          where: {
+            organizationId_userId: {
+              organizationId: organization.id,
+              userId: transferToUserId,
+            },
+          },
+        })
+
+        if (!transferToMembership) {
+          throw new BadRequestError('Membro alvo não encontrado.')
+        }
+
+        // Se uma transação falhar, as duas operações serão revertidas
+        await prisma.$transaction([
+          // Atualiza o membro alvo para ADMIN
+          prisma.member.update({
+            where: {
+              organizationId_userId: {
+                organizationId: organization.id,
+                userId: transferToUserId,
+              },
+            },
+            data: {
+              role: 'ADMIN',
+            },
+          }),
+
+          // Atualizar a propriedade da organização para o membro alvo
+          prisma.organization.update({
+            where: {
+              id: organization.id,
+            },
+            data: {
+              ownerId: transferToUserId,
+            },
+          }),
+        ])
+
+        return reply.status(204).send()
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -23,6 +23,7 @@ import { getMembership } from './routes/org/get-membership'
 import { getOrganization } from './routes/org/get-organization'
 import { getOrganizations } from './routes/org/get-organizations'
 import { shutdownOrganization } from './routes/org/shutdown-organization'
+import { transferOrganization } from './routes/org/transfer-organization'
 import { updateOrganization } from './routes/org/update-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -78,6 +79,7 @@ app.register(getOrganization)
 app.register(getOrganizations)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
+app.register(transferOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('> 🌟 O servidor está ativo e pronto para servir! 💾')
