f31bdd212917f9ebc7684c7dd3e608025f6255e4
Hilquias Ferreira Melo <hilquiasfmelo@gmail.com>
2024-11-30 15:43:27 -0300

    feat: :sparkles: Rota: Detalhes do convite

---
+++ apps/api/src/http/routes/invites/get-invite.ts
@@ -0,0 +1,74 @@
+import { roleSchema } from '@slave/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function getInvite(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/invites/:inviteId',
+    {
+      schema: {
+        tags: ['invites'],
+        summary: 'Ver um convite',
+        params: z.object({
+          inviteId: z.string(),
+        }),
+        response: {
+          200: z.object({
+            invite: z.object({
+              id: z.string().uuid(),
+              role: roleSchema,
+              email: z.string().email(),
+              createdAt: z.date(),
+              organization: z.object({
+                name: z.string(),
+              }),
+              author: z
+                .object({
+                  id: z.string().uuid(),
+                  name: z.string().nullable(),
+                  avatarUrl: z.string().url().nullable(),
+                })
+                .nullable(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { inviteId } = request.params
+
+      const invite = await prisma.invite.findUnique({
+        where: { id: inviteId },
+        select: {
+          id: true,
+          email: true,
+          role: true,
+          createdAt: true,
+          author: {
+            select: {
+              id: true,
+              name: true,
+              avatarUrl: true,
+            },
+          },
+          organization: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      })
+
+      if (!invite) {
+        throw new BadRequestError('Convite não encontrado')
+      }
+
+      return reply.status(200).send({ invite })
+    },
+  )
+}
