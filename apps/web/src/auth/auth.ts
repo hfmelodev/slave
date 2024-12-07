f6f512b4618d1d41f79dfc75623233dc96b1fc96
Hilquias Ferreira Melo <hilquiasfmelo@gmail.com>
2024-12-07 10:14:33 -0300

    feat: :sparkles: Obtendo permissões do usuário

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,7 +1,9 @@
+import { defineAbilityFor } from '@slave/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 import { getProfile } from '@/http/auth/getProfile'
+import { getMembership } from '@/http/org/getMembership'
 
 export async function isAuthenticated() {
   const cookieStore = await cookies()
@@ -9,6 +11,41 @@ export async function isAuthenticated() {
 
   // Retorna um boolean indicando se o usuário está autenticado
   return !!token
+}
+
+export async function getCurrentOrg() {
+  const cookieStore = await cookies()
+
+  return cookieStore.get('org')?.value ?? null
+}
+
+// Busca os afiliados de uma organização
+export async function getCurrentMembership() {
+  const org = await getCurrentOrg()
+
+  if (!org) {
+    return null
+  }
+
+  const { membership } = await getMembership(org)
+
+  return membership
+}
+
+// Busca as informações do usuário autenticado e quais permissões ele tem
+export async function ability() {
+  const membership = await getCurrentMembership()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  })
+
+  return ability
 }
 
 export async function auth() {
--- apps/web/src/components/app/header.tsx
+++ apps/web/src/components/app/header.tsx
@@ -1,17 +1,23 @@
 import { Blocks, Slash } from 'lucide-react'
 
+import { ability } from '@/auth/auth'
+
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
-export function Header() {
+export async function Header() {
+  const permission = await ability()
+
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Blocks className="size-6" />
 
-        <Slash className="size-3 -rotate-[26deg] text-muted-foreground" />
+        <Slash className="size-3 -rotate-[26deg] text-border" />
 
         <OrganizationSwitcher />
+
+        {permission?.can('get', 'Project') && <p>Projetos</p>}
       </div>
 
       <div className="flex items-center gap-4">
