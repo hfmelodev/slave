13b3f4b12103ca17755f1113b96715c89d6cf536
Hilquias Ferreira Melo <hilquiasfmelo@gmail.com>
2024-12-08 10:21:38 -0300

    feat: :sparkles: Comportamento do Sheet

--- apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
+++ apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
@@ -1,16 +1,12 @@
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-} from '@/components/ui/sheet'
+import { InterceptedSheetContent } from '@/components/app/intercepted-sheet-content'
+import { Sheet, SheetHeader, SheetTitle } from '@/components/ui/sheet'
 
 import { OrganizationForm } from '../../create-organization/components/organization-form'
 
 export default function CreateOrganization() {
   return (
     <Sheet defaultOpen>
-      <SheetContent>
+      <InterceptedSheetContent>
         <SheetHeader>
           <SheetTitle>Criar organização</SheetTitle>
         </SheetHeader>
@@ -18,7 +14,7 @@ export default function CreateOrganization() {
         <div className="w-full py-4">
           <OrganizationForm />
         </div>
-      </SheetContent>
+      </InterceptedSheetContent>
     </Sheet>
   )
 }
